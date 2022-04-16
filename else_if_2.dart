77951648063aebf1c4d8b4a2ad0475463ad021c6
Abdullah Javaid <abdullahjavaid4545@gmail.com>
2022-04-16 16:53:32 +0500

    dart program to find numbers of days in month

---
+++ else_if_2.dart
@@ -0,0 +1,17 @@
+void main(){
+  int month = 2;
+  var monthIn31 = ['January','March','May','July','Augest','October','December'];
+  var monthIn30 = ['April','September','November'];
+  // if((month == 1)&&(month == 3)&&(month == 5)&&(month == 7)&&(month == 8)&&(month == 9)&&(month == 12)){
+  // }
+  if(month >0 && month <7){
+    for(var months in monthIn30){
+if(month >0 && month < 3){
+      print('${monthIn30[month-1]} has 30 days');
+      break;
+    }
+    }
+    
+  print('${monthIn31[month-1]} has 31 days');
+  }
+}
